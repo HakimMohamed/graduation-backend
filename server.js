b063bce18bed55e672a9755349a8281629ed4d35
HakimMohamed <iabdelhakimmohamed@gmail.com>
2023-10-24 10:43:24 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// Import the Express module
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
