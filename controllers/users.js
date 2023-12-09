3f1ac4e230ed12a328621a5a6ff88978ecea419f
HakimMohamed <iabdelhakimmohamed@gmail.com>
2023-12-09 10:11:06 +0200

    add forget password to users

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,6 @@ userController.register = async (req, res) => {
 
 		const user = await User.findOne({ email });
 		if (user) {
-			console.log('already');
 			return res.status(200).json({ msg: 'Exists' });
 		}
 
@@ -23,7 +22,7 @@ userController.register = async (req, res) => {
 		const newUser = { name, password: hashedPassword, email };
 		await User.create(newUser);
 
-		const token = jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: '1h' });
+		const token = jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: '365d' });
 		console.log('login');
 		return res.status(200).json({ msg: 'User Created Successfully', token: token });
 	} catch (err) {
@@ -47,7 +46,7 @@ userController.login = async (req, res) => {
 		const hashedPassword = await bcrypt.compare(password, user.password);
 
 		if (hashedPassword) {
-			const token = jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: '1h' });
+			const token = jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: '365d' });
 			return res.status(200).json({ msg: 'Success', token: token });
 		} else {
 			return res.status(200).json({ msg: 'Invalid username or password.' });
@@ -57,6 +56,43 @@ userController.login = async (req, res) => {
 	}
 };
 
+userController.validateUser = async (req, res) => {
+	const { email } = req.body;
+	try {
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			return res.status(200).json({ status: 'failed', msg: 'Email does not exist.' });
+		}
+		return res.status(200).json({ status: 'success', msg: 'Success' });
+	} catch (err) {
+		res.status(400).json({ err: err.message });
+	}
+};
+
+userController.changePassword = async (req, res) => {
+	try {
+		const { password, confirmPassword, email } = req.body;
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			return res.status(200).json({ status: 'failed', msg: 'Email does not exist.' });
+		}
+
+		if (password !== confirmPassword) {
+			return res.status(200).json({ status: 'failed', msg: 'Password and confirm password does not match.' });
+		}
+
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		await User.updateOne({ _id: user._id }, { password: hashedPassword });
+
+		const token = jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: '365d' });
+		return res.status(200).json({ status: 'success', msg: 'Password Changed Successfully.', token: token });
+	} catch (err) {
+		res.status(400).json({ err: err.message });
+	}
+};
 userController.logout = async (req, res) => {
 	// later
 };
